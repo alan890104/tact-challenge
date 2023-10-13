b7847dcf04680882b47d9880931c3a39841ed0aa
alan890104 <alan890104@gmail.com>
2023-10-14 02:15:16 +0800

    feat: solve 1

--- tests/Task1.spec.ts
+++ tests/Task1.spec.ts
@@ -1,5 +1,6 @@
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
 import { toNano } from 'ton-core';
+import '@ton-community/test-utils';
 import { Task1 } from '../wrappers/Task1';
 
 describe('Task1', () => {
@@ -28,6 +29,45 @@ describe('Task1', () => {
         });
     });
 
+    function getRandomInt(min: number, max: number): number {
+        // Make sure min and max are integers
+        min = Math.floor(min);
+        max = Math.floor(max);
+
+        // Generate a random number between [min, max]
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function getRandomMethod(): 'Add' | 'Subtract' {
+        const options = ['Add', 'Subtract'];
+        const randomIndex = Math.floor(Math.random() * options.length);
+        return options[randomIndex] as 'Add' | 'Subtract';
+    }
+
     it('test', async () => {
+        const testCases = 1000;
+        for (let i = 0; i < testCases; i++) {
+            const randInt = getRandomInt(-1000, 1000);
+            const randMethod = getRandomMethod();
+            const beforeCounter = await task1.getCounter();
+            const deployer = await blockchain.treasury('deployer');
+            const result = await task1.send(
+                deployer.getSender(),
+                {
+                    value: toNano('0.05'),
+                },
+                {
+                    $$type: randMethod,
+                    queryId: 0n,
+                    number: BigInt(randInt),
+                }
+            );
+            expect(result.transactions).toHaveTransaction({
+                from: deployer.address,
+                to: task1.address,
+                success: true,
+            });
+            expect(await task1.getCounter()).toEqual(beforeCounter + 1n);
+        }
     });
 });
